6f0b018cda4807d462f10f704d3d4e7c9d36fc1a
Jincheng Zhang <zhjch05@live.com>
2018-11-16 23:09:33 -0500

    title and favicon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Editor from "./components/Editor";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount = () => {
+    document.title = "PPS 2018 Project 4 Map Editor";
+  };
+
   render() {
     return (
       <div className="App">
